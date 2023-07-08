c000e9a57d966f9b657bee23f97cd35edc792ce6
Foretack <observer.in.a.box@gmail.com>
2023-07-08 10:13:46 +0200

    Add enums

---
+++ Enums/CommandPermission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Enums;
+internal enum CommandPermission
+{
+    None,
+    Everyone,
+    Subscribers,
+    VIPs,
+    Moderators,
+    Whitelisted
+}
