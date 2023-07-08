c3256fa24de54051d06889106bf528a47b14a330
Foretack <observer.in.a.box@gmail.com>
2023-07-08 10:12:30 +0200

    Add interfaces

---
+++ Interfaces/IChatCommand.cs
@@ -0,0 +1,9 @@
+﻿using MiniTwitch.Irc.Models;
+
+namespace Bot.Interfaces;
+
+internal interface IChatCommand
+{
+    public CommandInfo Info { get; }
+    public ValueTask Run(Privmsg message);
+}
---
+++ Interfaces/IModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Interfaces;
+
+internal interface IModule
+{
+    public bool Enabled { get; }
+    public ValueTask Enable();
+    public ValueTask Disable();
+}
