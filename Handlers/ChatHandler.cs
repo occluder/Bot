d96e1e956f5dba3c3ae91fe65625bb203f7ea9ad
Foretack <observer.in.a.box@gmail.com>
2023-07-08 10:28:41 +0200

    Add handlers

---
+++ Handlers/ChatHandler.cs
@@ -0,0 +1,58 @@
+﻿using Bot.Interfaces;
+using Bot.Utils;
+using MiniTwitch.Irc.Models;
+
+namespace Bot.Handlers;
+
+public static class ChatHandler
+{
+    private static Dictionary<string, IChatCommand> _commands = new();
+
+    public static void Setup()
+    {
+        MainClient.OnMessage += OnMessage;
+        AnonClient.OnMessage += OnMessage;
+
+        LoadCommands();
+    }
+
+    private static void LoadCommands()
+    {
+        Type interfaceType = typeof(IChatCommand);
+        foreach (Type type in interfaceType.Assembly.GetTypes().Where(t => interfaceType.IsAssignableFrom(t) && !t.IsInterface))
+        {
+            if (Activator.CreateInstance(type) is IChatCommand command)
+            {
+                _commands.Add(command.Info.Name, command);
+                Debug("Loaded command: {CommandName}", command.Info.Name);
+            }
+        }
+
+        Information("ChatHandler dynamically loaded {CommandCount} commands", _commands.Count);
+    }
+
+    private static ValueTask OnMessage(Privmsg arg)
+    {
+        ReadOnlySpan<char> content = arg.Content;
+        if (ChannelsById[arg.Channel.Id].Priority >= 50 && content.Length > Config.Prefix.Length + 1
+            && content.StartsWith(Config.Prefix, StringComparison.CurrentCulture))
+        {
+            return HandleCommand(arg);
+        }
+
+        return default;
+    }
+
+    private static ValueTask HandleCommand(Privmsg message)
+    {
+        ReadOnlySpan<char> content = message.Content;
+        foreach (KeyValuePair<string, IChatCommand> kvp in _commands)
+        {
+            ReadOnlySpan<char> key = kvp.Key;
+            if (content[Config.Prefix.Length..].StartsWith(key) && message.Permits(kvp.Value) && !message.IsOnCooldown(kvp.Value))
+                return kvp.Value.Run(message);
+        }
+
+        return default;
+    }
+}
