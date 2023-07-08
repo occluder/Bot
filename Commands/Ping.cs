f3685ccd62fbbb93776ffb140e3bddb309669a0a
Foretack <observer.in.a.box@gmail.com>
2023-07-08 10:29:25 +0200

    Add commands

---
+++ Commands/Ping.cs
@@ -0,0 +1,17 @@
+﻿using Bot.Enums;
+using Bot.Interfaces;
+using Bot.Models;
+using MiniTwitch.Irc.Models;
+
+namespace Bot.Commands;
+
+internal class Ping : IChatCommand
+{
+    public CommandInfo Info => new("ping", "Ping", TimeSpan.FromSeconds(5), CommandPermission.Everyone);
+
+    public ValueTask Run(Privmsg message)
+    {
+        TimeSpan latency = DateTimeOffset.Now - message.SentTimestamp;
+        return MainClient.SendMessage(message.Channel.Name, $"FeelsDankMan Pong {latency.TotalMilliseconds}ms");
+    }
+}
