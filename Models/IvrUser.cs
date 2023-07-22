cf90ea2b49fc5667ef6b9b54281c3479f6bb7b14
Foretack <observer.in.a.box@gmail.com>
2023-07-22 14:36:24 +0200

    Trim IvrUser

--- Models/IvrUser.cs
+++ Models/IvrUser.cs
@@ -43,59 +43,9 @@ public readonly record struct IvrUser
     [JsonPropertyName("verifiedBot")]
     public object VerifiedBot { get; init; }
 
-    [JsonPropertyName("createdAt")]
-    public DateTime CreatedAt { get; init; }
-
-    [JsonPropertyName("updatedAt")]
-    public DateTime UpdatedAt { get; init; }
-
     [JsonPropertyName("emotePrefix")]
     public string EmotePrefix { get; init; }
-
-    [JsonPropertyName("roles")]
-    public UserRoles Roles { get; init; }
-
-    [JsonPropertyName("badges")]
-    public List<object> Badges { get; init; }
-
-    [JsonPropertyName("chatterCount")]
-    public int ChatterCount { get; init; }
-
-    [JsonPropertyName("chatSettings")]
-    public UserChatSettings ChatSettings { get; init; }
-
     [JsonPropertyName("stream")]
     public object Stream { get; init; }
-
-    [JsonPropertyName("lastBroadcast")]
-    public UserLastBroadcast LastBroadcast { get; init; }
-
-    [JsonPropertyName("panels")]
-    public List<UserPanel> Panels { get; init; }
-
-    public readonly record struct UserChatSettings(
-        [property: JsonPropertyName("chatDelayMs")] int ChatDelayMs,
-        [property: JsonPropertyName("followersOnlyDurationMinutes")] object FollowersOnlyDurationMinutes,
-        [property: JsonPropertyName("slowModeDurationSeconds")] object SlowModeDurationSeconds,
-        [property: JsonPropertyName("blockLinks")] bool BlockLinks,
-        [property: JsonPropertyName("isSubscribersOnlyModeEnabled")] bool IsSubscribersOnlyModeEnabled,
-        [property: JsonPropertyName("isEmoteOnlyModeEnabled")] bool IsEmoteOnlyModeEnabled,
-        [property: JsonPropertyName("isFastSubsModeEnabled")] bool IsFastSubsModeEnabled,
-        [property: JsonPropertyName("isUniqueChatModeEnabled")] bool IsUniqueChatModeEnabled,
-        [property: JsonPropertyName("requireVerifiedAccount")] bool RequireVerifiedAccount,
-        [property: JsonPropertyName("rules")] IReadOnlyList<object> Rules
-    );
-    public readonly record struct UserLastBroadcast(
-        [property: JsonPropertyName("startedAt")] DateTime StartedAt,
-        [property: JsonPropertyName("title")] string Title
-    );
-    public readonly record struct UserPanel(
-        [property: JsonPropertyName("id")] string Id
-    );
-    public readonly record struct UserRoles(
-        [property: JsonPropertyName("isAffiliate")] bool IsAffiliate,
-        [property: JsonPropertyName("isPartner")] bool IsPartner,
-        [property: JsonPropertyName("isStaff")] object IsStaff
-    );
 }
 
