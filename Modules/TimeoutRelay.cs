1b7b4e0441dc8b6cd4dc4ac12c5afe205c432ac0
Foretack <observer.in.a.box@gmail.com>
2023-07-27 10:54:47 +0200

    Update TimeoutRelay

--- Modules/TimeoutRelay.cs
+++ Modules/TimeoutRelay.cs
@@ -1,4 +1,5 @@
 ﻿using Bot.Interfaces;
+using Bot.Utils;
 using MiniTwitch.PubSub.Interfaces;
 using MiniTwitch.PubSub.Models;
 
@@ -7,32 +8,90 @@ namespace Bot.Modules;
 internal class TimeoutRelay : IModule
 {
     private static readonly ILogger _logger = ForContext<TimeoutRelay>();
+    private static readonly HttpClient _requests = new();
 
     public bool Enabled { get; private set; }
 
-    private ValueTask OnTimedOut(UserId userId, ITimeOutData data)
+    private async ValueTask OnTimedOut(UserId userId, ITimeOutData data)
     {
         if (data.ExpiresInMs <= 1000)
+        {
             _logger.Debug("You were timed out for {TimeoutDuration}s in #{Channel}: {Reason}", 1, ChannelNameOrId(data.ChannelId), data.Reason);
-        else
-            _logger.Information("You were timed out for {TimeoutDuration} in #{Channel}: {Reason}", TimeSpan.FromMilliseconds(data.ExpiresInMs), ChannelNameOrId(data.ChannelId), data.Reason);
+            return;
+        }
 
-        return default;
+        _logger.Information("You were timed out for {TimeoutDuration} in #{Channel}: {Reason}", 
+            TimeSpan.FromMilliseconds(data.ExpiresInMs), ChannelNameOrId(data.ChannelId), data.Reason);
+
+        string durationString = TimeSpan.FromMilliseconds(Math.Ceiling((double)data.ExpiresInMs)) switch
+        {
+            { TotalDays: >= 1 } ts => $"{ts.Days}d",
+            { TotalHours: >= 1 } ts => $"{ts.Hours}h",
+            { TotalMinutes: >= 2 } ts => $"{ts.Minutes}m",
+            { TotalSeconds: >= 1 } ts => $"{ts}s",
+            _ => $"{data.ExpiresInMs}ms",
+        };
+
+        var message = new DiscordMessageBuilder().AddEmbed(e =>
+        {
+            e.title = $"You were timed out for {durationString} in #{ChannelNameOrId(data.ChannelId)}: {data.Reason ?? "NO REASON"}";
+            e.color = 12767488;
+            e.timestamp = DateTime.Now;
+        });
+
+        await SendDiscordMessage(message);
     }
-    private ValueTask OnBanned(UserId userId, IBanData data)
+    private async ValueTask OnBanned(UserId userId, IBanData data)
     {
         _logger.Information("You were banned in #{Channel}: {Reason}", ChannelNameOrId(data.ChannelId), data.Reason);
-        return default;
+        var message = new DiscordMessageBuilder().AddEmbed(e =>
+        {
+            e.title = $"You were banned in #{ChannelNameOrId(data.ChannelId)}: {data.Reason ?? "NO REASON"}";
+            e.color = 16001024;
+            e.timestamp = DateTime.Now;
+        });
+
+        await SendDiscordMessage(message);
     }
-    private ValueTask OnUntimedOut(UserId userId, IUntimeOutData data)
+    private async ValueTask OnUntimedOut(UserId userId, IUntimeOutData data)
     {
         _logger.Information("You were untimed out in #{Channel}", ChannelNameOrId(data.ChannelId));
-        return default;
+        var message = new DiscordMessageBuilder().AddEmbed(e =>
+        {
+            e.title = $"You were untimed out in #{ChannelNameOrId(data.ChannelId)}";
+            e.color = 6353920;
+            e.timestamp = DateTime.Now;
+        });
+
+        await SendDiscordMessage(message);
     }
-    private ValueTask OnUnbanned(UserId userId, IUntimeOutData data)
+    private async ValueTask OnUnbanned(UserId userId, IUntimeOutData data)
     {
         _logger.Information("You were unbanned in #{Channel}", ChannelNameOrId(data.ChannelId));
-        return default;
+        var message = new DiscordMessageBuilder().AddEmbed(e =>
+        {
+            e.title = $"You were unbanned in #{ChannelNameOrId(data.ChannelId)}";
+            e.color = 6353920;
+            e.timestamp = DateTime.Now;
+        });
+
+        await SendDiscordMessage(message);
+    }
+
+    private static async Task SendDiscordMessage(DiscordMessageBuilder message)
+    {
+        try
+        {
+            HttpResponseMessage response = await _requests.PostAsync(Config.Links["MentionsWebhook"], message.ToStringContent());
+            if (response.IsSuccessStatusCode)
+                _logger.Debug("[{StatusCode}] POST {Url}", response.StatusCode, Config.Links["MentionsWebhook"]);
+            else
+                _logger.Warning("[{StatusCode}] POST {Url}", response.StatusCode, Config.Links["MentionsWebhook"]);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "POST {Url}", Config.Links["MentionsWebhook"]);
+        }
     }
 
     private static object ChannelNameOrId(long channelId)
