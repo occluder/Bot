a4971d7fb66c1e9e192afe144600b707854b0909
Foretack <observer.in.a.box@gmail.com>
2023-08-21 14:01:05 +0200

    Add sub collector

---
+++ Modules/SubCollector.cs
@@ -0,0 +1,77 @@
+﻿using Bot.Models;
+using Bot.Utils;
+using CachingFramework.Redis.Contracts.RedisObjects;
+using MiniTwitch.Irc.Interfaces;
+
+namespace Bot.Modules;
+
+public class SubCollector : BotModule
+{
+    private static IRedisSet<Sub> Subs => Collections.GetRedisSet<Sub>("bot:chat:sub_notices");
+    private readonly BackgroundTimer _timer;
+
+    public SubCollector()
+    {
+        _timer = new(TimeSpan.FromHours(1), Commit, PostgresTimerSemaphore);
+    }
+
+    private async ValueTask OnSub(ISubNotice notice)
+    {
+        if (!ChannelsById[notice.Channel.Id].IsLogged)
+            return;
+
+        Sub sub = new(
+            notice.Author.Name,
+            notice.Author.Id,
+            notice.Channel.Name,
+            notice.Channel.Id,
+            notice.CumulativeMonths,
+            notice.SubPlan.ToString(),
+            notice.SentTimestamp.DateTime
+        );
+
+        await Subs.AddAsync(sub);
+    }
+
+    private async Task Commit()
+    {
+        if (!base.Enabled || await Subs.GetCountAsync() == 0)
+            return;
+
+        Sub[] subs = Subs.ToArray();
+        try
+        {
+            int inserted = await Postgres.ExecuteAsync("insert into collected_subs values (@FromUser, @FromUserId, @ToChannel, @ToChannelId, @CumulativeMonths, @Tier, @TimeSent)", subs);
+            await Subs.ClearAsync();
+        }
+        catch (Exception ex)
+        {
+            ForContext<SubCollector>().Error(ex, "Failed to insert sub logs");
+        }
+    }
+
+    protected override ValueTask OnModuleEnabled()
+    {
+        MainClient.OnSubscriptionNotice += OnSub;
+        AnonClient.OnSubscriptionNotice += OnSub;
+        _timer.Start();
+        return default;
+    }
+
+    protected override async ValueTask OnModuleDisabled()
+    {
+        MainClient.OnSubscriptionNotice -= OnSub;
+        AnonClient.OnSubscriptionNotice -= OnSub;
+        await _timer.StopAsync();
+    }
+
+    private readonly record struct Sub(
+        string FromUser,
+        long FromUserId,
+        string ToChannel,
+        long ToChannelId,
+        int CumulativeMonths,
+        string Tier,
+        DateTime TimeSent
+    );
+}
