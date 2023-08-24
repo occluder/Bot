bc546758c1c197b8d32fa7974dbac31b0f485c1d
Foretack <observer.in.a.box@gmail.com>
2023-08-24 22:27:07 +0200

    Update

--- Modules/ChatUtils.cs
+++ Modules/ChatUtils.cs
@@ -7,19 +7,32 @@ public class ChatUtils: BotModule
 {
     private static ValueTask OnMessage(Privmsg message)
     {
-        if (message.Channel.Id != 11148817)
+        if (message.Channel.Id is not 11148817 and not 780092850)
             return default;
 
         string[] args = message.Content.Split(' ');
         return args[0] switch
         {
             "utc" => message.ReplyWith(DateTime.UtcNow.ToString("O")),
-            "unix" => message.ReplyWith(DateTimeOffset.Now.ToUnixTimeSeconds().ToString()),
-            { Length: >= 10 and < 13 } unix when long.TryParse(unix, out long time) => message.ReplyWith(DateTimeOffset.FromUnixTimeSeconds(time).ToString()),
-            { Length: >= 13 } unixMs when long.TryParse(unixMs, out long time) => message.ReplyWith(DateTimeOffset.FromUnixTimeMilliseconds(time).ToString()),
-            { Length: >= 28 } date when DateTimeOffset.TryParse(date, out var dateTime) => message.ReplyWith(dateTime.ToUnixTimeMilliseconds().ToString()),
+            "unix" => message.ReplyWith(DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString()),
+            { Length: >= 10 and < 13 } unix when long.TryParse(unix, out long time) && WithinReasonableTime(time) =>
+                message.ReplyWith(DateTimeOffset.FromUnixTimeSeconds(time).ToString("O")),
+
+            { Length: >= 13 } unixMs when long.TryParse(unixMs, out long time) && WithinReasonableTime(time, true) =>
+                message.ReplyWith(DateTimeOffset.FromUnixTimeMilliseconds(time).ToString("O")),
+
+            { Length: >= 28 } date when DateTimeOffset.TryParse(date, out var dateTime) => message.ReplyWith(
+                dateTime.ToUnixTimeMilliseconds().ToString()),
+
             _ => default
         };
+    }
+
+    private static bool WithinReasonableTime(long time, bool ms = false)
+    {
+        int year = DateTime.Now.Year;
+        var date = ms ? DateTimeOffset.FromUnixTimeMilliseconds(time) : DateTimeOffset.FromUnixTimeSeconds(time);
+        return date.Year <= year + 5 || date.Year >= year - 5;
     }
 
     protected override ValueTask OnModuleEnabled()
