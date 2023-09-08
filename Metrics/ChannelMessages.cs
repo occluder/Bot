0b517aef0d28959a52ce9e974456061991fc281a
Foretack <observer.in.a.box@gmail.com>
2023-09-08 15:51:53 +0200

    Add channel messages metric

---
+++ Metrics/ChannelMessages.cs
@@ -0,0 +1,45 @@
+﻿using Bot.Interfaces;
+using MiniTwitch.Irc.Models;
+
+namespace Bot.Metrics;
+
+public class ChannelMessages: IMetric
+{
+    private readonly Dictionary<long, int> _messageCount = new();
+    private uint _invc;
+
+    private ChannelMessages()
+    {
+        AnonClient.OnMessage += OnMessage;
+        MainClient.OnMessage += OnMessage;
+    }
+
+    private ValueTask OnMessage(Privmsg arg)
+    {
+        _messageCount.TryAdd(arg.Channel.Id, 0);
+        _messageCount[arg.Channel.Id]++;
+        return default;
+    }
+
+    public async Task Report()
+    {
+        if (++_invc % 20 != 0)
+            return;
+
+        await PostgresQueryLock.WaitAsync();
+        try
+        {
+            (string Channel, int MessageCount)[] values =
+                _messageCount.Select(kvp => (ChannelsById[kvp.Key].Username, kvp.Value)).ToArray();
+
+            await Postgres.ExecuteAsync("insert into metrics_channel_messages values (@Channel, @MessageCount)",
+                values);
+        }
+        finally
+        {
+            PostgresQueryLock.Release();
+        }
+
+        _messageCount.Clear();
+    }
+}
