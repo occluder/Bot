84c331b110fec90f56f779b3d054e4856221177c
Foretack <observer.in.a.box@gmail.com>
2023-09-15 10:07:48 +0200

    Cache timezones in ChatUtils.cs, reduce formatting operations

--- Modules/ChatUtils.cs
+++ Modules/ChatUtils.cs
@@ -5,6 +5,9 @@ namespace Bot.Modules;
 
 public class ChatUtils: BotModule
 {
+    private const int MAX_YEAR_OFFSET = 10;
+    private static readonly Dictionary<double, TimeZoneInfo> _timeZones = new();
+
     private static ValueTask OnMessage(Privmsg message)
     {
         string[] args = message.Content.Split(' ', StringSplitOptions.RemoveEmptyEntries);
@@ -22,6 +25,7 @@ public class ChatUtils: BotModule
             "utc" or "gmt" => message.ReplyWith(Date()),
             "et" or "edt" => message.ReplyWith(Date(-4)),
             "pt" or "pdt" => message.ReplyWith(Date(-7)),
+            "pst" => message.ReplyWith(Date(-8)),
             "unix" => message.ReplyWith(DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString()),
             { Length: >= 10 and < 13 } unix when long.TryParse(unix, out long time) && WithinReasonableTime(time) =>
                 message.ReplyWith(Date(unix: time)),
@@ -43,20 +47,26 @@ public class ChatUtils: BotModule
             var offset = ms
                 ? DateTimeOffset.FromUnixTimeMilliseconds(unix.Value)
                 : DateTimeOffset.FromUnixTimeSeconds(unix.Value);
-            return $"{offset:yyyy-M-d} {offset:h:mm:ss tt} [{offset:O}]";
+
+            return $"{offset:yyyy-M-d h:mm:ss tt} [{offset:O}]";
         }
 
         var utc = DateTimeOffset.UtcNow;
-        TimeZoneInfo tz = TimeZoneInfo.GetSystemTimeZones().First(t => t.BaseUtcOffset.TotalHours == hourOffset);
+        if (!_timeZones.TryGetValue(hourOffset, out TimeZoneInfo? tz))
+        {
+            tz = TimeZoneInfo.GetSystemTimeZones().First(t => t.BaseUtcOffset.TotalHours == hourOffset);
+            _timeZones.Add(hourOffset, tz);
+        }
+
         var date = TimeZoneInfo.ConvertTime(utc, tz);
-        return $"{date:yyyy-M-d} {date:h:mm:ss tt (zz)} [{date:O}]";
+        return $"{date:yyyy-M-d h:mm:ss tt (zz)} [{date:O}]";
     }
 
     private static bool WithinReasonableTime(long time, bool ms = false)
     {
         int year = DateTime.Now.Year;
         var date = ms ? DateTimeOffset.FromUnixTimeMilliseconds(time) : DateTimeOffset.FromUnixTimeSeconds(time);
-        return date.Year <= year + 5 && date.Year >= year - 5;
+        return date.Year <= year + MAX_YEAR_OFFSET && date.Year >= year - MAX_YEAR_OFFSET;
     }
 
     protected override ValueTask OnModuleEnabled()
