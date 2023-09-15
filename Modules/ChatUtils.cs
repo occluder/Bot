9c4a6d20da8cc44c14730134e51e2e9ff80618d9
Foretack <observer.in.a.box@gmail.com>
2023-09-15 10:14:11 +0200

    Catch parsing exceptions in ChatUtils.cs

--- Modules/ChatUtils.cs
+++ Modules/ChatUtils.cs
@@ -14,30 +14,39 @@ public class ChatUtils: BotModule
         if (args.Length > 1)
             return default;
 
-        return args[0] switch
+        try
         {
-            "aest" => message.ReplyWith(Date(10)),
-            "acst" => message.ReplyWith(Date(9.5)),
-            "awst" => message.ReplyWith(Date(8)),
-            "eest" or "ast" => message.ReplyWith(Date(3)),
-            "cest" or "eet" => message.ReplyWith(Date(2)),
-            "cet" => message.ReplyWith(Date(1)),
-            "utc" or "gmt" => message.ReplyWith(Date()),
-            "et" or "edt" => message.ReplyWith(Date(-4)),
-            "pt" or "pdt" => message.ReplyWith(Date(-7)),
-            "pst" => message.ReplyWith(Date(-8)),
-            "unix" => message.ReplyWith(DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString()),
-            { Length: >= 10 and < 13 } unix when long.TryParse(unix, out long time) && WithinReasonableTime(time) =>
-                message.ReplyWith(Date(unix: time)),
+            return args[0] switch
+            {
+                "aest" => message.ReplyWith(Date(10)),
+                "acst" => message.ReplyWith(Date(9.5)),
+                "awst" => message.ReplyWith(Date(8)),
+                "eest" or "ast" => message.ReplyWith(Date(3)),
+                "cest" or "eet" => message.ReplyWith(Date(2)),
+                "cet" => message.ReplyWith(Date(1)),
+                "utc" or "gmt" => message.ReplyWith(Date()),
+                "et" or "edt" => message.ReplyWith(Date(-4)),
+                "pt" or "pdt" => message.ReplyWith(Date(-7)),
+                "pst" => message.ReplyWith(Date(-8)),
+                "unix" => message.ReplyWith(DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString()),
+                { Length: >= 10 and < 13 } unix when long.TryParse(unix, out long time) && WithinReasonableTime(time) =>
+                    message.ReplyWith(Date(unix: time)),
 
-            { Length: >= 13 } unixMs when long.TryParse(unixMs, out long time) && WithinReasonableTime(time, true) =>
-                message.ReplyWith(Date(unix: time, ms: true)),
+                { Length: >= 13 } unixMs when long.TryParse(unixMs, out long time) && WithinReasonableTime(time, true)
+                    => message.ReplyWith(Date(unix: time, ms: true)),
 
-            { Length: >= 19 } date when DateTimeOffset.TryParse(date, out DateTimeOffset dateTime) => message.ReplyWith(
-                dateTime.ToUnixTimeMilliseconds().ToString()),
+                { Length: >= 19 } date when DateTimeOffset.TryParse(date, out DateTimeOffset dateTime) => message
+                    .ReplyWith(
+                        dateTime.ToUnixTimeMilliseconds().ToString()),
 
-            _ => default
-        };
+                _ => default
+            };
+        }
+        catch (Exception e)
+        {
+            ForContext<ChatUtils>().Warning(e, "Parsing exception: ");
+            return ValueTask.CompletedTask;
+        }
     }
 
     private static string Date(double hourOffset = 0, long? unix = null, bool ms = false)
