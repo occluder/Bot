4a419cdf36c8299ea73a752e7d6029356e758bfc
Foretack <observer.in.a.box@gmail.com>
2023-10-27 13:56:50 +0200

    Add test module

---
+++ Modules/Test.cs
@@ -0,0 +1,39 @@
+﻿using Bot.Models;
+using MiniTwitch.Helix.Models;
+using MiniTwitch.Helix.Responses;
+using MiniTwitch.Irc.Models;
+
+namespace Bot.Modules;
+
+public class Test: BotModule
+{
+    protected override ValueTask OnModuleEnabled()
+    {
+        AnonClient.OnMessage += OnMessage;
+        return default;
+    }
+
+    protected override ValueTask OnModuleDisabled()
+    {
+        AnonClient.OnMessage -= OnMessage;
+        return default;
+    }
+
+    private static async ValueTask OnMessage(Privmsg arg)
+    {
+        if (Random.Shared.Next(1000) != 10)
+            return;
+
+        HelixResult<BannedUser> result = await HelixClient.BanUser(Config.Ids["ParentId"], Config.Ids["BotId"], new()
+        {
+            Data = new()
+            {
+                UserId = arg.Author.Id,
+                Duration = TimeSpan.FromSeconds(Random.Shared.Next(1000)),
+                Reason = $"xD {arg.GetHashCode()}"
+            }
+        });
+
+        Information("{@BanResult}", result);
+    }
+}
