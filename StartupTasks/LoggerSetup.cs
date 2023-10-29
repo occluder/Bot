81e9ec884910e14d70e6ff4ded5a0a2356b785dc
Foretack <observer.in.a.box@gmail.com>
2023-10-29 19:57:07 +0100

    Remove loki

--- StartupTasks/LoggerSetup.cs
+++ StartupTasks/LoggerSetup.cs
@@ -5,7 +5,6 @@ using Serilog.Core;
 using Serilog.Enrichers.ClassName;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
-using Serilog.Sinks.Grafana.Loki;
 
 namespace Bot.StartupTasks;
 
@@ -26,13 +25,6 @@ public class LoggerSetup: IStartupTask
             .WriteTo.File(new CompactJsonFormatter(), "data.log", flushToDiskInterval: TimeSpan.FromMinutes(2.5), rollingInterval: RollingInterval.Month)
             .WriteTo.File("readable_data.log", flushToDiskInterval: TimeSpan.FromMinutes(2.5), rollingInterval: RollingInterval.Month)
             .WriteTo.Discord(Config.Links["Webhook"])
-            .WriteTo.GrafanaLoki("http://localhost:3100", new[]
-            {
-                new LokiLabel { Key = "Application", Value = "Bot" }
-            }, new[]
-            {
-                "SourceContext", "ClassName"
-            })
             .CreateLogger();
 
         return ValueTask.FromResult(StartupTaskState.Completed);
