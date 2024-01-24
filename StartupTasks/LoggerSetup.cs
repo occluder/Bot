a88278f08ecf167ca97c9f270833b9cba47ac94d
Foretack <69414142+Foretack@users.noreply.github.com>
2024-01-24 12:02:55 +0100

    no further file logs

--- StartupTasks/LoggerSetup.cs
+++ StartupTasks/LoggerSetup.cs
@@ -4,7 +4,6 @@ using Bot.Utils.Logging;
 using Serilog.Core;
 using Serilog.Enrichers.ClassName;
 using Serilog.Events;
-using Serilog.Formatting.Compact;
 
 namespace Bot.StartupTasks;
 
@@ -22,8 +21,6 @@ public class LoggerSetup: IStartupTask
                 outputTemplate:
                 "[{Timestamp:yyyy.mm.dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}{NewLine}",
                 levelSwitch: LogSwitch)
-            .WriteTo.File(new CompactJsonFormatter(), "data.log", flushToDiskInterval: TimeSpan.FromMinutes(2.5), rollingInterval: RollingInterval.Month)
-            .WriteTo.File("readable_data.log", flushToDiskInterval: TimeSpan.FromMinutes(2.5), rollingInterval: RollingInterval.Month)
             .WriteTo.Discord(Config.Links["Webhook"])
             .CreateLogger();
 
