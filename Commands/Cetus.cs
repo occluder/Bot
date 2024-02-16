7fa97f2b9f9c6efdb5fda694f8051f878653a4f0
Foretack <69414142+Foretack@users.noreply.github.com>
2024-02-16 21:42:00 +0100

    fix

--- Commands/Cetus.cs
+++ Commands/Cetus.cs
@@ -7,9 +7,6 @@ namespace Bot.Commands;
 
 public class Cetus: ChatCommand
 {
-    private long _checkAfter;
-    private bool _isDay;
-
     public override CommandInfo Info { get; } = new(
         "cetus",
         string.Empty,
@@ -19,28 +16,14 @@ public class Cetus: ChatCommand
 
     public override async ValueTask Run(Privmsg message)
     {
-        if (Unix() < _checkAfter)
-        {
-            Verbose("Cetus cycle is still active. Day: {IsDay}", _isDay);
-            var diff = TimeSpan.FromSeconds(_checkAfter - Unix());
-            Verbose("It is currently {Cycle}", _isDay ? "day" : "night");
-            Verbose("Time until new cycle: {Expiry}", PrettyTimeString(diff));
-            string cycleString = $"{(_isDay ? "a" : "b")} {PrettyTimeString(diff)}";
-            await message.ReplyWith(cycleString);
-            return;
-        }
-
-        Verbose("Cetus cycle expired. Fetching new cycle...");
         OneOf<CetusCycle, HttpStatusCode, Exception> response =
             await GetFromRequest<CetusCycle>("https://api.warframestat.us/pc/cetusCycle");
 
+        const string sun = "\u2600\ufe0f";
+        const string moon = "\ud83c\udf19";
         Verbose("Fetched new cetus cycle");
-        await response.Match(cycle =>
-            {
-                _isDay = cycle.isDay;
-                _checkAfter = cycle.expiry.ToUnixTimeSeconds();
-                return Run(message);
-            },
+        await response.Match(
+            cycle => message.ReplyWith($"{(cycle.isDay ? moon : sun)} in {cycle.timeLeft}"),
             statusCode => message.ReplyWith($"Received bad status code: {statusCode} :("),
             exception => message.ReplyWith($"Exception handling code: ({exception.GetType().Name}) {exception.Message}")
         );
