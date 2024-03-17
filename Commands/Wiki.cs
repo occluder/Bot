dfa88a95900775d0fe8d19851e8c012782fb36d6
Foretack <69414142+Foretack@users.noreply.github.com>
2024-03-17 06:51:37 +0100

    Add wiki command for warframe

---
+++ Commands/Wiki.cs
@@ -0,0 +1,26 @@
+﻿using Bot.Enums;
+using Bot.Models;
+using MiniTwitch.Irc.Models;
+
+namespace Bot.Commands;
+
+public class Wiki: ChatCommand
+{
+    public override CommandInfo Info { get; } = new(
+        "wiki",
+        "Search the Warframe wiki",
+        TimeSpan.FromSeconds(1),
+        CommandPermission.Everyone
+    );
+
+    public Wiki()
+    {
+        AddArgument(new("SearchTerm", 1, typeof(string)));
+    }
+
+    public override ValueTask Run(Privmsg message)
+    {
+        string[] s = message.Content.Split(' ');
+        return message.ReplyWith($"https://antifandom.com/warframe/search?q={string.Join('+', s[1..])}");
+    }
+}
