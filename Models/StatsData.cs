734ef492ab7fb0600e0f9f7d9602dfa26a221f89
Foretack <69414142+Foretack@users.noreply.github.com>
2024-05-07 22:05:18 +0200

    ok

--- Models/StatsData.cs
+++ Models/StatsData.cs
@@ -2,15 +2,12 @@
 
 namespace Bot.Models;
 
-public record StatsData(StatsPayload Payload);
-public record StatsPayload(PeriodStats StatisticsClosed);
+public record StatsData([property: JsonPropertyName("payload")] StatsPayload Payload);
+public record StatsPayload([property: JsonPropertyName("statistics_closed")] PeriodStats StatisticsClosed);
 public record PeriodStats([property: JsonPropertyName("48hours")] Statistic[] _48hours);
-public record Statistic(float Median, float MovingAvg, int Volume, DateTime Datetime);
-
-public record Data(ListingsPayload Payload);
-public record ListingsPayload(Order[] BuyOrders, Order[] SellOrders);
-public record Order(int Platinum, int Quantity, ItemInfo Item);
-
-public record ItemInfo(LocalizedName En);
-
-public record LocalizedName(string ItemName);
+public record Statistic(
+    [property: JsonPropertyName("median")] float Median,
+    [property: JsonPropertyName("moving_avg")] float MovingAvg,
+    [property: JsonPropertyName("volume")] int Volume,
+    [property: JsonPropertyName("datetime")] DateTime Datetime
+);
