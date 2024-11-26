c7824335184af76a9e27ae6730f1c33821029722
Foretack <69414142+Foretack@users.noreply.github.com>
2024-11-26 13:52:18 +0300

    Update memory usage metric

--- Metrics/MemoryUsage.cs
+++ Metrics/MemoryUsage.cs
@@ -1,10 +1,67 @@
-﻿using Bot.Interfaces;
+﻿using System.Diagnostics;
+using Bot.Interfaces;
 
 namespace Bot.Metrics;
 
 public class MemoryUsage: IMetric
 {
-    private const string KEY = "bot:metrics:memory_usage";
+    private uint _invc;
 
-    public Task Report() => RedisDatabaseAsync.StringSetAsync(KEY, GC.GetTotalMemory(false), TimeSpan.FromMinutes(5));
+    public async Task Report()
+    {
+        if (++_invc % 20 != 0)
+            return;
+
+        await PostgresQueryLock.WaitAsync();
+        try
+        {
+            long ts = Unix();
+            object[] metrics =
+            [
+                new
+                {
+                    Measurement = "GC_Total",
+                    Bytes = GC.GetTotalMemory(false),
+                    Ts = ts
+                },
+                new
+                {
+                    Measurement = "GC_Allocated",
+                    Bytes = GC.GetTotalAllocatedBytes(),
+                    Ts = ts
+                },
+                new
+                {
+                    Measurement = "Process_Private",
+                    Bytes = Process.GetCurrentProcess().PrivateMemorySize64,
+                    Ts = ts
+                },
+                new
+                {
+                    Measurement = "Process_Working",
+                    Bytes = Process.GetCurrentProcess().WorkingSet64,
+                    Ts = ts
+                },
+                new
+                {
+                    Measurement = "Process_PeakWorking",
+                    Bytes = Process.GetCurrentProcess().PeakWorkingSet64,
+                    Ts = ts
+                },
+            ];
+
+            await Postgres.ExecuteAsync(
+                "insert into metrics_memory values (@Measurement, @Bytes, @Ts)",
+                metrics
+            );
+        }
+        catch (Exception ex)
+        {
+            ForContext<MemoryUsage>().Error(ex, "Something went wrong {InvocationCount}", _invc);
+        }
+        finally
+        {
+            PostgresQueryLock.Release();
+        }
+    }
 }
