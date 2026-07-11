794e2456a433f6a7d828b1ab2907539ef0ac7c6c
occluder <69414142+occluder@users.noreply.github.com>
2026-07-11 17:26:26 +0300

    Fix BackgroundTimer deadlock on StopAsync; add CancellationTokenSource

--- Utils/BackgroundTimer.cs
+++ Utils/BackgroundTimer.cs
@@ -5,7 +5,7 @@ internal class BackgroundTimer
     private static readonly ILogger _logger = ForContext<BackgroundTimer>();
     private readonly PeriodicTimer _timer;
     private readonly Func<Task> _callback;
-    private readonly double _minsUntilComplete;
+    private readonly CancellationTokenSource _cts = new();
     private Task? _timerTask;
     private nint _invocationsLeft;
 
@@ -15,45 +15,49 @@ internal class BackgroundTimer
             throw new ArgumentException("Max invocation count can't be 0", nameof(maxInvocationCount));
 
         _timer = new PeriodicTimer(period);
-        _minsUntilComplete = maxInvocationCount * period.TotalMinutes;
         _callback = callback;
         _invocationsLeft = maxInvocationCount;
         _logger.Debug("New background timer created ({InvocationCount} * {Period})", maxInvocationCount, period);
     }
 
-    public void Start() => _timerTask = _minsUntilComplete > 10 ? Task.Factory.StartNew(StartTimerTask, TaskCreationOptions.LongRunning) : StartTimerTask();
+    public void Start() => _timerTask = Task.Run(StartTimerTask);
 
     public async Task StopAsync()
     {
         if (_timerTask is null)
-        {
             return;
-        }
 
+        _cts.Cancel();
+        _timer.Dispose();
         try
         {
             await _timerTask;
         }
-        finally
+        catch (OperationCanceledException)
         {
-            _timerTask.Dispose();
         }
 
-        _logger.Information("Background timer aborted");
+        _logger.Information("Background timer stopped");
     }
 
     private async Task StartTimerTask()
     {
-        while (_invocationsLeft != 0 && await _timer.WaitForNextTickAsync())
+        try
         {
-            try
+            while (_invocationsLeft != 0 && await _timer.WaitForNextTickAsync(_cts.Token))
             {
-                await _callback();
+                try
+                {
+                    await _callback();
+                }
+                finally
+                {
+                    _invocationsLeft--;
+                }
             }
-            finally
-            {
-                _invocationsLeft--;
-            }
+        }
+        catch (OperationCanceledException)
+        {
         }
     }
 }
