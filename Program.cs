6b46a8aa3fe97d84d29447014feee186d9e60dd4
occluder <69414142+occluder@users.noreply.github.com>
2026-07-11 17:25:54 +0300

    Always park on Task.Delay(-1); remove SERVICE env var and Console.ReadLine loop

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using Bot.Enums;
 using Bot.StartupTasks;
-using Bot.Utils.Logging;
-using Serilog.Events;
 
 namespace Bot;
 
@@ -31,42 +29,6 @@ internal class Program
             if (result != StartupTaskState.Completed)
                 throw new NotSupportedException(result.ToString());
 
-        if (Environment.GetEnvironmentVariable("SERVICE") is "TRUE")
-        {
-            Console.WriteLine("Running as a service -- Console input is disabled.");
-            await Task.Delay(-1);
-            return;
-        }
-
-        while (true)
-        {
-            string? input = Console.ReadLine();
-            if (string.IsNullOrEmpty(input))
-                continue;
-
-            if (Enum.TryParse(input, true, out LogEventLevel level))
-            {
-                await Settings.ChangeLogLevel((int)level);
-                Console.WriteLine($"Switching logging level to: {level}");
-            }
-            else if (input == "clear")
-            {
-                Console.Clear();
-            }
-            else if (input.StartsWith("l!include"))
-            {
-                string[] args = input.Split(' ');
-                if (args.Length < 2)
-                    continue;
-
-                ClassNameFilter.ClassName = args[1];
-                Console.WriteLine($"ClassNameFilter set to {args[1]}");
-            }
-            else if (input == "l!unfilter")
-            {
-                ClassNameFilter.ClassName = null;
-                Console.WriteLine("Removed all filters");
-            }
-        }
+        await Task.Delay(-1);
     }
 }
